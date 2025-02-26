22041d404e8eedc58b549a6a7c7088995f396af7
sjMun09 <ohoh7391@naver.com>
2025-02-26 16:54:36 +0900

    refactor:lv2.N+1문제

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,8 +1,10 @@
 package org.example.expert.domain.todo.repository;
 
+import jakarta.persistence.Entity;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,12 +13,12 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    @Query("select t from Todo t order by t.modifiedAt desc")
+    @EntityGraph(attributePaths = {"user"}, type =  EntityGraph.EntityGraphType.LOAD)
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN FETCH t.user " +
-            "WHERE t.id = :todoId")
+    @Query("select t from Todo t where t.id = :todoId")
+    @EntityGraph(attributePaths = {"user"}, type =  EntityGraph.EntityGraphType.LOAD)
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     int countById(Long todoId);
